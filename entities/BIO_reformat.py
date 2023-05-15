949a365593ba4a250300e4487f6c3388d7e5a7b7
Lsar <lukas.ssarka@gmail.com>
2023-05-15 13:11:36 +0200

    Add files via upload

--- entities/BIO_reformat.py
+++ entities/BIO_reformat.py
@@ -1,16 +1,21 @@
 import os
+
 def reformat_files(directory):
+    new_directory = "reformatted_generated_entities"
+    os.makedirs(new_directory, exist_ok=True)
+
     for filename in os.listdir(directory):
         if filename.endswith(".txt"):
             tag = filename.split('.')[0]  # Extract the tag from the filename
             file_path = os.path.join(directory, filename)
+            new_file_path = os.path.join(new_directory, f"{tag}.txt")  # New file path with tag name
 
             # Read the file and split the lines into words
             with open(file_path, 'r', encoding='utf-8') as f:
                 entities = [line.strip().split() for line in f if line.strip()]
 
-            # Write the words back to the file, each on a new line, with a corresponding BIO tag
-            with open(file_path, 'w', encoding='utf-8') as f:
+            # Write the words back to the new file, each on a new line, with a corresponding BIO tag
+            with open(new_file_path, 'w', encoding='utf-8') as f:
                 for entity in entities:
                     for i, word in enumerate(entity):
                         # Assign the 'B-' tag to the first word and 'I-' to the rest
@@ -19,4 +24,4 @@ def reformat_files(directory):
                     f.write("\n")  # Add an empty line after each entity
 
 if __name__ == '__main__':
-    reformat_files('./generated_tags/')
+    reformat_files('./generated_entities/')
